64138441381c7c28c98a6f3149a74f5b76f7e0fd
AndreyKUBIK <a.kubasov07@mail.ru>
2026-01-01 10:02:56 +0000

    upd2

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from flask import Flask
-from flask_restx import Resource, Api
+from flask_restx import Api, Resource, fields
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(
+    app,
+    title="Todo API",
+    description="GET и PUT",
+)
 
-@api.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+todos = {}
+
+todo_model = api.model('Todo', {
+    'data': fields.String(required=True, description='Текст задачи')
+})
+
+
+@api.route('/todo/<string:todo_id>')
+class Todo(Resource):
+
+    @api.response(200, 'Задача найдена')
+    @api.response(404, 'Задача не найдена')
+    def get(self, todo_id):
+        if todo_id not in todos:
+            api.abort(404, f"Задача {todo_id} не найдена")
+
+        return {
+            "todo_id": todo_id,
+            "data": todos[todo_id]
+        }
+
+
+    @api.expect(todo_model)
+    @api.response(200, 'Задача сохранена')
+    def put(self, todo_id):
+        todos[todo_id] = api.payload['data']
+
+        return {
+            "todo_id": todo_id,
+            "data": todos[todo_id]
+        }
+
 
 if __name__ == '__main__':
     app.run(debug=True)
